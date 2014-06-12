db5094ea74567e7648cd14b53949b2322bf05ecd
Kevin Verdieck <kverdieck@palantir.com>
2014-06-11 21:08:38 -0700

    Removed old fetch methodology

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Configuration files to quickly setup a new personalEnvironment
 
 To install, run the following:
 ```
-wget https://raw.github.com/nomcopter/pEnv/master/fetch.sh -O /tmp/fetch.sh
-bash /tmp/fetch.sh
+git clone https://github.com/nomcopter/pEnv ~/.pEnv
+~/.pEnv/install.sh
 ```
--- fetch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-(command -v vim > /dev/null && command -v git > /dev/null) || { echo "ERROR: Both vim and git must be installed to continue"; exit 1; }
-
-read -p "Have you installed your ssh key into github? [yN] " ynInstall
-[ "$ynInstall" == "y" ] && url="git@github.com:nomcopter/pEnv.git" || url="https://github.com/nomcopter/pEnv.git"
-
-git clone $url ~/.pEnv
-
-exec ~/.pEnv/install.sh
