78ca30c65b7a865fe493651060b8c04c11610e22
Kevin Verdieck <kevin@ironcladapp.com>
2018-03-05 15:35:08 -0800

    Don't try to reinstall existing pieces

--- install.sh
+++ install.sh
@@ -3,23 +3,23 @@
 # clone-or-pull repo-location folder-destination
 clone-or-pull() 
 {
-    if [ -d $2 ]; then
-        echo "Plugin already installed, updating..."
-        cd $2
-        git pull
-    else
-        git clone $1 $2
-    fi
+  if [ -d $2 ]; then
+    echo "Plugin already installed, updating..."
+    cd $2
+    git pull
+  else
+    git clone $1 $2
+  fi
 }
 
 # backup-and-link asset dest
 backup-and-link()
 {
-    if [ -e $2 ] && [ ! -L $2 ]; then
-        mv $2{,.bak}
-    fi
-    rm -f $2
-    ln -s $1 $2
+  if [ -e $2 ] && [ ! -L $2 ]; then
+    mv $2{,.bak}
+  fi
+  rm -f $2
+  ln -s $1 $2
 }
 
 mkdir -p ~/local/bin
@@ -27,15 +27,19 @@ git submodule init
 git submodule update
 
 case $OSTYPE in 
-darwin*)
+  darwin*)
     echo
-    echo "Installing Homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    if ! [ -x "$(command -v brew)" ]; then
+      echo "Installing Homebrew..."
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
 
     echo "Brewing packages..."
     brew install git tmux coreutils the_silver_searcher fzf vim reattach-to-user-namespace
-    # Detect intelligently
-    #$(brew --prefix)/opt/fzf/install
+    if [ ! -f ~/.fzf.zsh ]; then
+      $(brew --prefix)/opt/fzf/install --key-bindings --completion
+    fi
 
     echo "Linking utils..."
     backup-and-link /usr/local/bin/gdircolors ~/local/bin/dircolors
@@ -44,7 +48,7 @@ darwin*)
     # TODO: Setup fonts
     # TODO: Install/Setup iTerm2
     ;;
-linux*)
+  linux*)
     echo
     echo "Setting up fonts..."
     backup-and-link ~/.pEnv/assets/fonts ~/.fonts
@@ -65,9 +69,9 @@ backup-and-link ~/.pEnv/assets/git-number/git-list    ~/local/bin/git-list
 backup-and-link ~/.pEnv/assets/git-number/git-id      ~/local/bin/git-id
 
 if [ ! -d ~/.oh-my-zsh ]; then
-    echo
-    echo "Installing oh-my-zsh..."
-    curl -kL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+  echo
+  echo "Installing oh-my-zsh..."
+  curl -kL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 fi
 
 echo
@@ -76,20 +80,20 @@ mkdir -p ~/.oh-my-zsh/custom/plugins
 clone-or-pull https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 if [ ! -f ~/.pEnv/assets/rcs/gitconfig ]; then
-    echo
-    echo "Setting up Git..."
-    echo "Enter your personal information for commits"
-    read -p " Name: " name
-    read -p "Email: " email
-    sed -e "s/{{NAME}}/$name/" -e "s/{{EMAIL}}/$email/" ~/.pEnv/assets/gitconfig.template > ~/.pEnv/assets/rcs/gitconfig
+  echo
+  echo "Setting up Git..."
+  echo "Enter your personal information for commits"
+  read -p " Name: " name
+  read -p "Email: " email
+  sed -e "s/{{NAME}}/$name/" -e "s/{{EMAIL}}/$email/" ~/.pEnv/assets/gitconfig.template > ~/.pEnv/assets/rcs/gitconfig
 fi
 
 echo
 echo "Installing RC files..."
 for path in ~/.pEnv/assets/rcs/*
 do
-    name=$(basename $path)
-    backup-and-link $path ~/.$name
+  name=$(basename $path)
+  backup-and-link $path ~/.$name
 done
 
 echo
